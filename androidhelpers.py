a7d1ae16094bd544ff8c81939c7551bb927145b1
Gabriel Pettier <dev@tangibledisplay.com>
2014-12-04 12:48:56 +0000

    remove debug prints, only print data in onLeScan if no callback is set

--- androidhelpers.py
+++ androidhelpers.py
@@ -1,21 +1,15 @@
 from jnius import PythonJavaClass, java_method, autoclass
-print "jnius imports!"
 
 # SERVICE = Autoclass('org.renpy.PythonService').mService
 SERVICE = autoclass('org.renpy.android.PythonActivity').mActivity
-print "get activity"
 
 Context = autoclass('android.content.Context')
-print "get Context"
 
 Intent = autoclass('android.content.Intent')
-print "get Intent class"
 
 BluetoothManager = SERVICE.getSystemService(Context.BLUETOOTH_SERVICE)
-print "get BluetoothManager"
 
 ADAPTER = BluetoothManager.getAdapter()
-print "get ADAPTER"
 
 REQUEST_ENABLE_BT = 0x100
 
@@ -26,22 +20,15 @@ def activity_result(request_code, result_code, data):
 
 
 def start_scanning(callback):
-    print "start scanning"
     if not ADAPTER.isEnabled():
-        print "not enabled!"
         SERVICE.startActivityForResult(
             Intent(ADAPTER.ACTION_REQUEST_ENABLE), REQUEST_ENABLE_BT)
-        print "started activity for result"
         SERVICE.bind(on_activity_result=activity_result)
-        print "binded on_activity_result"
     else:
-        print "enabled! start leScan"
         ADAPTER.startLeScan(callback)
-        print "scan enabled!"
 
 
 def stop_scanning(callback):
-    print "stop leScan"
     ADAPTER.stopLeScan(callback)
 
 
@@ -56,9 +43,11 @@ class AndroidScanner(PythonJavaClass):
 
     @java_method("(Landroid/bluetooth/BluetoothDevice;I[B)V")
     def onLeScan(self, device, irssi, scan_record):
-        print "onLeScan"
-        print device.getName()
-        print irssi
-        print len(scan_record), scan_record
         if self.callback:
             self.callback(device.getName(), device.getAddress(), irssi, scan_record)
+        else:
+            print "no callback set"
+            print "onLeScan"
+            print device.getName()
+            print irssi
+            print len(scan_record), scan_record
