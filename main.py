81129b6ba6bb9c6e14c5222cc3608467387608f3
gabriel pettier <dev@tangibledisplay.com>
2014-12-08 17:55:44 +0100

    fix filter value

--- main.py
+++ main.py
@@ -412,7 +412,7 @@ class BLEApp(App):
         self.root.ids.scan.add_widget(TwizSimulator())
 
     def filter_scan_result(self, result):
-        return 'twi' in result
+        return 'twi' in result.lower()
 
     def restart_scanning(self, dt):
         self.scanning_active = not self.scanning_active
